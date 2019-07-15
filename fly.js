cf7db55db0a7849ce468168cb0f5e2c51c99463f
minide <miriklnm@gmail.com>
2019-07-16 00:06:36 +0300

    add2

--- fly.js
+++ fly.js
@@ -1,8 +1,37 @@
 let db = {
-  travelList: [],
-  regList: []
+  travelList: []
 }
 
-let travel = function travel(name, price){
-  this.ID = db.travelList
+let Travel = function travel(name, price){
+
+  this.ID = db.travelList.length == 0 ? 1 :
+  db.travelList[db.travelList.length - 1].ID + 1;
+
+  this.Name = name;
+  this.Price = price;
+
 }
+
+db.travelList.push(new Travel("holand", 100))
+db.travelList.push(new Travel("israel", 50))
+
+let buildHTML = function buildHTML(object){
+  return  `
+        <div class="travel-items travel-id=${object.ID}">
+        <div class="destination">${object.Name}</div>
+        <div class="price">${object.Price}</div>
+        </div>
+   `
+
+}
+
+
+
+let finel = function puthtml(d){
+  for(let i = 0; i < db.travelList.length; i++){
+    let x = db.travelList[i];
+    let d = buildHTML(x);
+
+  }
+}
+console.log(final);
